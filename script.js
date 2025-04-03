0de91a3251ac1d02fb0a35b365bd6b321a5302c3
slezakIT <apps@slezak-it.de>
2025-04-03 10:22:18 +0200

    javascript todolist

---
+++ script.js
@@ -0,0 +1,69 @@
+const TODOS_FORM_EL = document.querySelector('#todos_add_form');
+const TODOS_FORM_NAME_EL = document.querySelector('#todo_name');
+const OPEN_TODOS_LIST_EL = document.querySelector('#open_todos_list');
+const CLOSED_TODOS_LIST_EL = document.querySelector('#closed_todos_list');
+
+let todoList = [];
+
+TODOS_FORM_EL.addEventListener('submit', processTodoFormSubmission);
+
+function processTodoFormSubmission(e) {
+    e.preventDefault();
+
+    let todoName = TODOS_FORM_NAME_EL.value.trim();
+
+    if(todoName.length === 0){
+        return; // Funktion ist durch return zu Ende
+    }
+
+    const todoItem = {
+        todoName: todoName,
+        isDone: false,
+        id: `todo-${Date.now()}-${Math.floor(Math.random()*9999)}`
+    }
+
+
+    todoList.push(todoItem);
+
+    renderTodoList();
+}
+
+
+function renderTodoList() {
+    //LIST reset
+    OPEN_TODOS_LIST_EL.innerHTML = "";
+    CLOSED_TODOS_LIST_EL.innerHTML = "";
+    //render open todos
+    const openTodos= todoList.filter((todoListItem)=>{
+        return !todoListItem.isDone
+    })
+
+    openTodos.forEach((todoListItem) => {
+        const LI_ELEMENT = document.createElement('LI');
+        LI_ELEMENT.innerHTML = todoListItem.todoName;
+        LI_ELEMENT.addEventListener("click", ()=> toggleTodo(todoListItem.id))
+        OPEN_TODOS_LIST_EL.appendChild(LI_ELEMENT);
+    })
+
+    // render closed todos
+    const closedTodos= todoList.filter((todoListItem)=>{
+        return todoListItem.isDone;
+    })
+    closedTodos.forEach((todoListItem) => {
+        const LI_ELEMENT = document.createElement('LI');
+        LI_ELEMENT.innerHTML = todoListItem.todoName;
+        LI_ELEMENT.classList.add('todo-done');
+        LI_ELEMENT.addEventListener("click", ()=> toggleTodo(todoListItem.id))
+        CLOSED_TODOS_LIST_EL.appendChild(LI_ELEMENT);
+    })
+}
+
+function toggleTodo(todoId){
+    todoList = todoList.map((todoListItem)=>{
+        if(todoListItem.id === todoId){
+            todoListItem.isDone = !todoListItem.isDone;
+        }
+        return todoListItem;
+    })
+    renderTodoList();
+}
